16c4e1ce428ce5661a4559a7359dda23c02309a7
tibssy <norbert.tiborcz@gmail.com>
2024-08-29 14:13:20 +0100

    Add embeddings

--- app.py
+++ app.py
@@ -4,22 +4,28 @@ from openai_helper import OpenAIChat
 
 st.title("💬 Chatbot")
 
+
+
+
 if 'chat' not in st.session_state:
     st.session_state.chat = OpenAIChat(openai_model='gpt-4o-mini')
     st.session_state.messages = [{"role": "assistant", "content": "How can I help you?"}]
-    print(f'new instance {st.session_state}')
+
+uploaded_file = st.file_uploader("Choose a document to upload")
+if uploaded_file is not None:
+    print(type(uploaded_file))
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
-    print(f'messages:\n{st.session_state}')
 
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
     response = st.session_state.chat.send_message(prompt)
-    print(response)
     message = response.get('text')
 
     st.session_state.messages.append({"role": "assistant", "content": message})
     st.chat_message("assistant").write(message)
+
+
--- openai_helper.py
+++ openai_helper.py
@@ -17,6 +17,7 @@ class OpenAIChat:
     ):
         self.openai_api_key = openai_api_key or os.getenv('OPENAI_API_KEY')
         self.openai_model = openai_model
+        self.assistant = None
         self.embedding_model = embedding_model
         self.url = 'https://api.openai.com'
         self.messages = [
@@ -26,9 +27,20 @@ class OpenAIChat:
             }
         ]
 
-    def post_request(self, endpoint: str, headers: dict, data: dict) -> dict:
+    def _post_request(self, endpoint: str, headers: dict, data: dict) -> dict:
         try:
             response = requests.post(f'{self.url}/{endpoint}', headers=headers, json=data, timeout=20)
+        except requests.exceptions.RequestException as err:
+            return {'status': f'Error: {err}'}
+        else:
+            result = {'status': response.status_code}
+            if result['status'] == 200:
+                result['json'] = response.json()
+            return result
+
+    def _get_request(self, endpoint: str, headers: dict) -> dict:
+        try:
+            response = requests.get(f'{self.url}/{endpoint}', headers=headers, timeout=20)
         except requests.exceptions.RequestException as err:
             return {'status': f'Error: {err}'}
         else:
@@ -55,7 +67,7 @@ class OpenAIChat:
             'response_format': {'type': 'json_object'},
             'messages': self.messages
         }
-        response = self.post_request(end_point, headers, data)
+        response = self._post_request(end_point, headers, data)
 
         if not (result := response.get('json')):
             return {'text': response.get('status')}
@@ -67,31 +79,32 @@ class OpenAIChat:
         else:
             return {'text': 'No response from OpenAI API'}
 
-    def get_embedding(self, text: str = 'hello') -> list | None:
+
+
+    def create_embeddings(self, text) -> list:
         endpoint = 'v1/embeddings'
         headers = {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {self.openai_api_key}"
         }
         data = {
-            "model": self.embedding_model,
-            "input": text
+            'input': text,
+            'model': 'text-embedding-3-small',
+            'encoding_format': 'float'
         }
-        response = self.post_request(endpoint, headers, data)
 
-        if not (result := response.get('json')):
-            return
+        response = self._post_request(endpoint, headers, data)
 
-        return result['data'][0]['embedding']
+        if not (res := response.get('json')):
+            return []
+
+        return res['data'][0]['embedding']
 
 
 def main():
-    openai = OpenAIChat()
+    openai = OpenAIChat(openai_model='gpt-4o-mini')
     # print(openai.send_message('Hello, how are you?'))
-    vector_data = openai.get_embedding()
-    print(vector_data)
-
-
+    print(openai.create_embeddings('hello'))
 
 
 if __name__ == '__main__':
